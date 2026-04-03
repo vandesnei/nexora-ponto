ee8d18ebfc6257e54cec6746ce9aa80493656e46
Vandesnei <vandesnei@hotmail.com>
2026-04-03 17:56:33 -0400

    fix: caminho do frontend correto

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ limit: "10mb", extended: true }))
 
 // ================= FRONTEND =================
-const frontendPath = path.join(__dirname, "frontend")
+const frontendPath = path.join(__dirname, "..", "frontend")
 
 app.use(express.static(frontendPath))
 app.use("/models", express.static(path.join(frontendPath, "models")))
